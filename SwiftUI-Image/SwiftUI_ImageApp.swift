df05383de40a68567cc21347f0572bb58f638c0b
Andrea Reinicke <andrea.reinicke@gmx.net>
2023-02-14 13:20:19 +0100

    Initial Commit

---
+++ SwiftUI-Image/SwiftUI_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ImageApp.swift
+//  SwiftUI-Image
+//
+//  Created by Andrea Reinicke on 14.02.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
